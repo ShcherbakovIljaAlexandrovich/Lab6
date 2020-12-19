69efaeaf5a6cea6a633dee14fd1090f974fb8229
Ilja <ilja@ylyxa.ylyxa.com>
2020-12-19 15:02:29 +0300

    Scripted auto-commit on change (2020-12-19 15:02:29) by gitwatch.sh

--- src/main/java/Anonymizer.java
+++ src/main/java/Anonymizer.java
@@ -8,25 +8,35 @@ import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.concurrent.CompletionStage;
+
+import static akka.http.javadsl.server.Directives.*;
 
 public class Anonymizer {
     public static Http http;
-    public static 
+    public static ActorRef configStorageActor;
+    private static final Duration timeout = Duration.ofSeconds(5);
+    public static ZooKeeper keeper;
 
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
-        ActorRef configStorageActor = system.actorOf(Props.create(ConfigStorageActor.class));
+        configStorageActor = system.actorOf(Props.create(ConfigStorageActor.class));
         final Http http = Http.get(system);
         final ActorMaterializer materializer =
                 ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-                createRoute(system, configStorageActor).flow(system, materializer);
+                createRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
@@ -43,5 +53,36 @@ public class Anonymizer {
         return http.singleRequest(HttpRequest.create(url));
     }
 
-    public static Route createRoute()
+    public static Route createRoute() {
+        return route(get(() ->
+            parameter("url", url ->
+                    parameter("count", count -> {
+                        if (Integer.parseInt(count) <= 0) {
+                            return completeWithFuture(fetch(url));
+                        }
+                        return completeWithFuture(Patterns.ask(configStorageActor, new GetNextServer(), timeout)
+                                .thenApply(nextServer -> (String)nextServer)
+                                .thenCompose(nextServer -> fetch(nextServer + "&count=" + count)));
+                    }))));
+    }
+
+    public static Watcher watcher = watchedEvent -> {
+        if (watchedEvent.getType() == Watcher.Event.EventType.NodeCreated ||
+            watchedEvent.getType() == Watcher.Event.EventType.NodeDeleted ||
+            watchedEvent.getType() == Watcher.Event.EventType.NodeDataChanged) {
+            ArrayList<String> newServers = new ArrayList<>();
+            try {
+                for (String s: keeper.getChildren("/servers", false, null)) {
+                    byte[] url = keeper.getData("/servers/" + s, false, null);
+                    newServers.add(new String(url));
+                }
+            } catch (KeeperException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    public static void initZooKeeper() {
+        
+    }
 }
