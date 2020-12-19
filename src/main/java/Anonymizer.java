78b405e34ec21b717665f2838c244f70738e3815
Ilja <ilja@ylyxa.ylyxa.com>
2020-12-19 14:31:26 +0300

    Scripted auto-commit on change (2020-12-19 14:31:26) by gitwatch.sh

--- src/main/java/Anonymizer.java
+++ src/main/java/Anonymizer.java
@@ -7,6 +7,7 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
@@ -14,6 +15,8 @@ import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
 public class Anonymizer {
+    public static Http http;
+
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
@@ -34,5 +37,9 @@ public class Anonymizer {
                 .thenAccept(unbound -> system.terminate());
     }
 
-    
+    private static CompletionStage<HttpResponse> fetch(String url) {
+        return http.singleRequest(HttpRequest.create(url));
+    }
+
+    public static Route
 }
