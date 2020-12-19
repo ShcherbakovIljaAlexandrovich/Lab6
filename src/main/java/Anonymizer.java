48c4645252973016156cd1b36edb14fadc77c20c
Ilja <ilja@ylyxa.ylyxa.com>
2020-12-19 14:13:28 +0300

    Scripted auto-commit on change (2020-12-19 14:13:28) by gitwatch.sh

--- src/main/java/Anonymizer.java
+++ src/main/java/Anonymizer.java
@@ -1,7 +1,36 @@
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class Anonymizer {
-    public static void main(String[] args) throws IOException{
-
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        ActorRef cachingActor = system.actorOf(Props.create(CachingActor.class));
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(materializer, cachingActor);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
